cbe12be4f720d5455ab30b58ebe1371e56b499c5
Andrew Day <Andrew Day>
2017-05-06 15:42:31 +1000

    add support for actions

--- app/redux-calculator-example.jsx
+++ app/redux-calculator-example.jsx
@@ -12,8 +12,43 @@ console.log('Starting redux...');
 
 var reducer = (state = stateDefault, action) => {
   //state = state || {name: "Anonymous"}
-//
-  return state;
+
+  switch(action.type)
+  {
+    case "CHANGE_DISPLAY" :
+      return {
+        ...state,
+        display : action.display
+      }
+    case "CHANGE_OPERATOR":
+      return {
+        ...state,
+        operator: action.operator
+      }
+    case "CHANGE_NUMBER_INIT":
+      return {
+        ...state,
+        currentValue: action.currentValue
+      }
+    case "CHANGE_NUMBER":
+      return {
+        ...state,
+        newValue: action.newValue
+      }
+    case "CHANGE_OPERATOR_MODE":
+      return {
+        ...state,
+        operatorMode: action.operatorMode
+      }
+    case "HANDLE_EQUALS":
+      return {
+        ...state
+      }
+    default:
+      return state;
+  }
+
+  console.log(action);
 }
 
 var store = redux.createStore(reducer);
@@ -21,3 +56,39 @@ var store = redux.createStore(reducer);
 var currentState = store.getState();
 
 console.log('currentState', currentState);
+
+
+
+store.dispatch({
+  type: "CHANGE_DISPLAY",
+  display: "123"
+});
+
+store.dispatch({
+  type: "CHANGE_OPERATOR",
+  operator: "+"
+});
+
+store.dispatch({
+  type: "CHANGE_NUMBER_INIT",
+  currentValue : 123
+});
+
+store.dispatch({
+  type: "CHANGE_NUMBER",
+    newValue: 456
+});
+
+store.dispatch({
+  type: "CHANGE_OPERATOR_MODE",
+    operatorMode: true
+});
+
+store.dispatch({
+  type: "HANDLE_EQUALS"
+});
+
+
+
+
+console.log('display should 123', store.getState());
